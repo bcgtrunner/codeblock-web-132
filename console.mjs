94553eccb27a3e8937778b8047d4427869d76018
bcgtrunner <210253731+bcgtrunner@users.noreply.github.com>
2026-02-22 16:48:38 +0700

    Web console

---
+++ console.mjs
@@ -0,0 +1,25 @@
+class Console
+{
+    constructor() {
+        this.output = document.querySelector(".console_output");
+        this.input = document.querySelector(".console_input");
+        this.input.addEventListener("keydown", (e) => {
+            if(e.key === "Enter")
+            {
+                this.execute();
+            }
+        });
+    }
+
+    print(text) {
+        this.output.innerHTML += `<div> ${text}</div>`;
+    }
+
+    execute() {
+        const text = this.input.value;
+        this.output.innerHTML += `<div>> ${text}</div>`;
+        this.output.innerHTML += `<div> Text entered: "${text}"</div>`;
+        this.input.value = '';
+    }
+}
+export {Console}
---
+++ webUi.mjs
@@ -0,0 +1,4 @@
+import { ASTNode, Interpreter } from "./index.mjs";
+import { Console } from "./console.mjs";
+const editorConsole = new Console();
+editorConsole.print("HELLO,WORLD!")
